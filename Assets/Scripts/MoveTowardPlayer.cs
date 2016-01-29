2aafcce2d3e9638d7259a02536832afaec4f4760
AlphaDreams <argann.b@hotmail.fr>
2016-01-30 00:41:43 +0100

    Ajout script rotate & move towardplayer

---
+++ Assets/Scripts/MoveTowardPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTowardPlayer : MonoBehaviour {
+
+	public GameObject player;
+
+	public float speed;
+
+
+	// Update is called once per frame
+	void Update () {
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards (transform.position, player.transform.position, step);
+	}
+}
