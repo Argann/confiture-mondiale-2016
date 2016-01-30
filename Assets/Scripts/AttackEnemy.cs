2213d51d0093c123b51bf50a9571e774e95cbb6e
Benjamin Robert <borsing.ingame@gmail.com>
2016-01-30 19:16:44 +0100

    add cooldown

--- Assets/Scripts/AttackEnemy.cs
+++ Assets/Scripts/AttackEnemy.cs
@@ -4,9 +4,12 @@ using System.Collections.Generic;
 
 public class AttackEnemy : MonoBehaviour {
 
-    public int damageAttack; 
+    public int damageAttack;
+    public float cooldown = 1f;
+    private float cooldownT ;
 
-	private List<GameObject> ennemy_list;
+
+    private List<GameObject> ennemy_list;
 	private Player player;
 
 
@@ -14,7 +17,8 @@ public class AttackEnemy : MonoBehaviour {
 	void Start(){
 		this.ennemy_list = new List<GameObject> ();
 		this.player = this.transform.parent.gameObject.GetComponent<Player>();
-	}
+        cooldownT = cooldown;
+    }
 
 	void OnTriggerEnter2D(Collider2D coll){
       
@@ -32,16 +36,17 @@ public class AttackEnemy : MonoBehaviour {
 	}
 
 	void Update(){
-		if (Input.GetMouseButtonDown (0)) {
-			Debug.Log("LELEL");
-			foreach(GameObject enemy in this.ennemy_list){
-				/*if(enemy == null){
-					this.ennemy_list.Remove(enemy);
-				} else {
-					enemy.GetComponent<Ennemy>().Blesser(damageAttack);
-				}*/
+        cooldownT -= Time.deltaTime;
+
+		if (Input.GetMouseButtonDown (0) && cooldownT <= 0) {
+            cooldownT = cooldown;
+            List<GameObject> ennemy_list2 = new List<GameObject>(this.ennemy_list);
+
+            foreach (GameObject enemy in ennemy_list2){
                 if(enemy != null){
                     enemy.GetComponent<Ennemy>().Blesser(damageAttack);
+                } else {
+                    this.ennemy_list.Remove(enemy);
                 }
 			}
 		}
---
+++ Assets/Scripts/CircleAOEAttack.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleAOEAttack : MonoBehaviour {
+
+    GameObject projectile;
+    public float cooldown = 1f;
+    private float cooldownT;
+
+    void Start () {
+        cooldownT = cooldown;
+    }
+	
+	void Update () {
+        cooldownT -= Time.deltaTime;
+
+        if (Input.GetMouseButtonDown(0) && cooldownT <= 0)
+        {
+            cooldownT = cooldown;
+            //List<GameObject> ennemy_list2 = new List<GameObject>(this.ennemy_list);
+            var p1 = Instantiate(projectile) as GameObject;
+            //p1.transform.SetParent(transform);
+            //((projectile)p1).
+           // p1.transform.localPosition = Vector3.zero;
+           
+        }
+    }
+}
