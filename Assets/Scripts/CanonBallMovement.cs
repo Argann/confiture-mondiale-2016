6c4b7c7837c18abc8da9ab95d7bb94f911af54ff
Etienne Cassin <Etienne Cassin>
2016-01-30 18:44:11 +0100

    Ajout des pieges

---
+++ Assets/Scripts/CanonBallMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanonBallMovement : MonoBehaviour {
+	private int timeout, timer;
+	// Use this for initialization
+	void Start () {
+		timeout = 100;
+		timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition = new Vector3 (transform.localPosition.x +0.1f, transform.localPosition.y, 0f);
+		timer++;
+		if (timer > timeout) {
+			Destroy (this.gameObject, 0f);
+		}
+	}
+}
--- Assets/Scripts/TouchPlayer.cs
+++ Assets/Scripts/TouchPlayer.cs
@@ -5,28 +5,40 @@ public class TouchPlayer : MonoBehaviour {
 
     public Player player;
 
-	private bool onSpike;
+	private bool onSpike, onFlame;
 
 	void Start() {
 		onSpike = false;
 	}
 	
 	void OnTriggerEnter2D(Collider2D hitbox) {
-		if (hitbox.gameObject.CompareTag("Enemy")) {
-            player.Touched(5);
-		} else if (hitbox.gameObject.CompareTag("Spike")) {
+		if (hitbox.gameObject.CompareTag ("Enemy")) {
+			player.Touched (5);
+		} else if (hitbox.gameObject.CompareTag ("Spike")) {
 			onSpike = true;
+		} else if (hitbox.gameObject.CompareTag ("FlameThrower")) {
+			onFlame = true;
+		} else if (hitbox.gameObject.CompareTag ("SlowZone")) {
+			player.speed /= 2;
+		} else if (hitbox.gameObject.CompareTag ("CanonBall")) {
+			player.Touched (35);
 		}
     }
 
 	void OnTriggerExit2D(Collider2D hitbox) {
 		if (hitbox.gameObject.CompareTag("Spike")) {
 			onSpike = false;
+		} else if (hitbox.gameObject.CompareTag("FlameThrower")) {
+			onFlame = false;
+		} else if (hitbox.gameObject.CompareTag ("SlowZone")) {
+			player.speed *= 2;
 		}
 	}
 
 	void Update() {
 		if (onSpike)
-			player.Touched (100);
+			player.Touched (1);
+		if (onFlame)
+			player.Touched (5);
 	}
 }
