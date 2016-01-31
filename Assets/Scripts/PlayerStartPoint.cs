51546afc12d6c0c213980ca014d05fd0c726305f
AlphaDreams <argann.b@hotmail.fr>
2016-01-31 10:57:57 +0100

    ojerv

---
+++ Assets/Scripts/PlayerStartPoint.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStartPoint : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		GameObject go = GameObject.FindGameObjectWithTag ("Player");
+		go.transform.position = this.transform.position;
+
+	}
+}
---
+++ Assets/Scripts/TeleportToAnotherScene.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TeleportToAnotherScene : MonoBehaviour
+{
+	public Image FadeImg;
+	public float fadeSpeed = 1.5f;
+	public string level;
+	private bool sceneStarting = true;
+	private bool sceneEnding = false;
+
+
+	void Awake()
+	{
+		FadeImg.rectTransform.localScale = new Vector2(Screen.width, Screen.height);
+	}
+
+	void Update()
+	{
+		// If the scene is starting...
+		if (sceneStarting)
+			// ... call the StartScene function.
+			StartScene();
+
+		if (sceneEnding)
+			EndScene ();
+	}
+
+
+	void FadeToClear()
+	{
+		// Lerp the colour of the image between itself and transparent.
+		FadeImg.color = Color.Lerp(FadeImg.color, Color.clear, fadeSpeed * Time.deltaTime);
+	}
+
+
+	void FadeToBlack()
+	{
+		// Lerp the colour of the image between itself and black.
+		FadeImg.color = Color.Lerp(FadeImg.color, Color.black, fadeSpeed * Time.deltaTime);
+	}
+
+
+	void StartScene()
+	{
+		// Fade the texture to clear.
+		FadeToClear();
+
+		// If the texture is almost clear...
+		if (FadeImg.color.a <= 0.05f)
+		{
+			// ... set the colour to clear and disable the RawImage.
+			FadeImg.color = Color.clear;
+			FadeImg.enabled = false;
+
+			// The scene is no longer starting.
+			sceneStarting = false;
+		}
+	}
+
+
+	public void EndScene()
+	{
+		// Make sure the RawImage is enabled.
+		FadeImg.enabled = true;
+
+		// Start fading towards black.
+		FadeToBlack();
+
+		// If the screen is almost black...
+		if (FadeImg.color.a >= 0.95f) {
+			// ... reload the level
+			GameObject go = GameObject.FindGameObjectWithTag ("PlayerPrefab");
+			DontDestroyOnLoad (go);
+			SceneManager.LoadScene (level, LoadSceneMode.Single);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll){
+		sceneEnding = true;
+	}
+}
