47678b4316ffa2b78bd0cf218964510de4b17175
Erwan AZZOUG <erwan.azzoug@etu.univ-nantes.fr>
2016-01-31 10:40:29 +0100

    Ajout gestion bouton (1ère partie)

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonManager : MonoBehaviour {
+
+    public GameObject player;
+
+	public void OnClickSkill(Object button)
+    {
+        switch (button.name)
+        {
+            case "b_1":
+                break;
+            case "b_2":
+                break;
+            case "b_3":
+                break;
+            case "b_4":
+                break;
+            case "b_5":
+                break;
+            default:
+                break;
+        }
+        //TODO
+    }
+}
