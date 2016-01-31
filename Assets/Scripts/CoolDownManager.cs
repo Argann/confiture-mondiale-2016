de4ec81e3049e8dce2cd592a9be8b732d61cb6d6
Benjamin Robert <borsing.ingame@gmail.com>
2016-01-31 01:16:34 +0100

    new prefabs + true Changements

--- Assets/Scripts/CoolDownManager.cs
+++ Assets/Scripts/CoolDownManager.cs
@@ -4,20 +4,27 @@ using System.Collections;
 public class CoolDownManager : MonoBehaviour {
     private SpriteRenderer cooldownBar;
     private float coolDown, coolDownT;
-    private Quaternion rotation;
+
     // Use this for initialization
     void Start () {
-        rotation = this.transform.rotation;
         cooldownBar = gameObject.GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        coolDown = this.transform.parent.gameObject.GetComponentInChildren<AttackEnemy>().cooldown;
-        coolDownT = this.transform.parent.gameObject.GetComponentInChildren<AttackEnemy>().cooldownT;
+        if (GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<AttackEnemy>() != null)
+        {
+            coolDown = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<AttackEnemy>().cooldown;
+            coolDownT = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<AttackEnemy>().cooldownT;
+        }
+        else
+        {
+            coolDown = GameObject.FindGameObjectWithTag("Player").GetComponent<AttackManager>().cooldown;
+            coolDownT = GameObject.FindGameObjectWithTag("Player").GetComponent<AttackManager>().cooldownT;
+        }
         if (this.coolDownT <= 0) coolDownT = 0;
-        //   this.cooldownBar.transform.rotation = rotation;
-        Vector3 position = GameObject.Find("Joueur").transform.position;
+
+        Vector3 position = GameObject.FindGameObjectWithTag("Player").transform.position;
         this.cooldownBar.transform.position = new Vector3(position.x,position.y-0.7f);
         this.cooldownBar.transform.localScale = new Vector3(coolDownT / coolDown, 1);
 
